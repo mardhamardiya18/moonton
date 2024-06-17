06794fe432179e1ab54fc5e7ef1e02e2f768ac40
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-06-17 22:24:39 +0800

    Slicing browse movies

--- resources/js/Pages/Prototype/Dashboard.jsx
+++ resources/js/Pages/Prototype/Dashboard.jsx
@@ -19,6 +19,7 @@ const Dashboard = () => {
         <Index>
             <Head>
                 <link rel="stylesheet" href="https://unpkg.com/flickity@2/dist/flickity.min.css" />
+                <title>Dashboard</title>
             </Head>
             <div>
                 <div className="font-semibold text-[22px] text-black mb-4">Featured Movies</div>
@@ -28,6 +29,32 @@ const Dashboard = () => {
                             <FeatureMovie key={i} slug={`the-batman-in-love`} name={`The Batman in Love ${i}`} category={`Action`} rating={i + 1} thumbnail={`/images/featured-1.png`} />
                         ))
                     }
+                </Flickity>
+            </div>
+
+            <div className='mt-[50px]'>
+                <div className="font-semibold text-[22px] text-black mb-4">Browse</div>
+                <Flickity className="gap-[30px]" options={flickityOptions}>
+
+                    {[1, 2, 3, 4, 5, 6].map(i => (
+                        <div className="absolute group overflow-hidden mr-[30px]" key={i}>
+                            <img src="/images/browse-1.png"
+                                className="object-cover rounded-[30px] h-[340px] w-[250px]" alt="" />
+                            <div
+                                className="absolute bottom-0 left-0 right-0 bg-gradient-to-t from-black rounded-bl-[28px] rounded-br-[28px]">
+                                <div className="px-7 pb-7">
+                                    <div className="font-medium text-xl text-white">Meong Golden</div>
+                                    <p className="mb-0 text-gray-300 text-base mt-[10px]">Horror • Love</p>
+                                </div>
+                            </div>
+                            <div className="absolute top-1/2 left-1/2 -translate-y-[500px] group-hover:-translate-y-1/2
+                                -translate-x-1/2 z-20 transition ease-in-out duration-500">
+                                <img src="/icons/ic_play.svg" className="" width="50" alt="" />
+                            </div>
+                            <a href="watching.html" className="inset-0 absolute z-50"></a>
+                        </div>
+                    ))}
+
                 </Flickity>
             </div>
 
