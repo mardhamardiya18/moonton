891fc4fab21f6d2f02772cd24064eb4257055415
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2024-06-22 22:37:26 +0800

    Integration Sidebar MenuItem

---
+++ resources/js/Layouts/Authenticated/MenuItem.jsx
@@ -0,0 +1,13 @@
+import { Link } from '@inertiajs/react'
+import React from 'react'
+
+const MenuItem = ({ link, isActive, icon, text, method = 'get' }) => {
+    return (
+        <Link href={link ? route(link) : null} className={`side-link ${isActive && 'active'}`} method={method} as='button'>
+            {icon}
+            {text}
+        </Link>
+    )
+}
+
+export default MenuItem
